d2e518ecca1dc53cdbb0852764cf5556ac2fcb11
Tanay Baviskar <tandy22kar@gmail.com>
2026-09-23 14:12:54 -0400

    prep for early deployment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 import fastf1
 import requests
 import numpy as np
